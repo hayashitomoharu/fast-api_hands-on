c32b4e9a5c427a4d5c20a3210f08d3b844eeea82
hayashitomoharu <146327155+hayashitomoharu@users.noreply.github.com>
2023-10-10 17:19:27 +0900

    関数名を命名規則に従ってfastapi側で使うものをindex〜〜にしました。

--- fastapi-sample2/main.py
+++ fastapi-sample2/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI,Depends,HTTPException
 
 from database_config import SessionLocal
-from crud_departments import (get_department as g_dept, get_departments as g_depts, insert_department as i_dept)
-from crud_users import ( get_users as g_users, get_user as g_user, insert_user as i_users)
+from crud_departments import (get_departments, get_department, insert_department)
+from crud_users import ( get_users, get_user, insert_user)
 from sqlalchemy.orm import Session
 import validation
 from typing import List
@@ -22,18 +22,18 @@ def get_db():
 
 # get
 @app.get("/departments",response_model=List[validation.Departments])
-async def get_departments(db:Session=Depends(get_db)):
+async def index_departments(db:Session=Depends(get_db)):
  
     """
         crud_departments.pyの関数でsqlを実行する
         http://localhost:8000/departments
     """
-    return g_depts(db=db)
+    return get_departments(db=db)
 
 
 
 @app.get("/departments/{departments_id}",response_model=validation.Departments)
-async def get_department(departments_id:int,db:Session=Depends(get_db)):
+async def index_department(departments_id:int,db:Session=Depends(get_db)):
     
     """
         crud_departments.pyの関数でsqlを実行する
@@ -41,7 +41,7 @@ async def get_department(departments_id:int,db:Session=Depends(get_db)):
         http://localhost:8000/departments/any
     """
     try:
-        result = g_dept(id=departments_id,db=db)
+        result = get_department(id=departments_id,db=db)
         if result == None:
             raise HTTPException(status_code=422, detail=f"{departments_id}:Negative values are not accepted")
         return result
@@ -50,7 +50,7 @@ async def get_department(departments_id:int,db:Session=Depends(get_db)):
 
 # insert
 @app.post("/departments",response_model=validation.Departments)
-async def insert_department(name:str,db:Session=Depends(get_db)):
+async def index_department(name:str,db:Session=Depends(get_db)):
     """
         crud_departments.pyの関数でsqlを実行する
         POSTで指定したnameでdepartmentsに新たに追加する
@@ -58,25 +58,25 @@ async def insert_department(name:str,db:Session=Depends(get_db)):
         curl -X POST "http://localhost:8000/departments?name=any" -H "accept: application/json"
 
     """
-    return i_dept(name=name,db=db)
+    return insert_department(name=name,db=db)
 
 
 #users
 
 # get
 @app.get("/users",response_model=List[validation.Users])
-async def get_users(db:Session=Depends(get_db)):
+async def index_users(db:Session=Depends(get_db)):
  
     """
         crud_users.pyの関数でsqlを実行する
         http://localhost:8000/users
     """
-    return g_users(db=db)
+    return get_users(db=db)
 
 
 
 @app.get("/users/{users_id}",response_model=validation.Users)
-async def get_user(users_id:int,db:Session=Depends(get_db)):
+async def index_user(users_id:int,db:Session=Depends(get_db)):
     
     """
         crud_users.pyの関数でsqlを実行する
@@ -84,7 +84,7 @@ async def get_user(users_id:int,db:Session=Depends(get_db)):
         http://localhost:8000/users/any
     """
     try:
-        result = g_user(id=users_id,db=db)
+        result = get_user(id=users_id,db=db)
         if result == None:
             raise HTTPException(status_code=422, detail=f"{users_id}:Negative values are not accepted")
         return result
@@ -94,7 +94,7 @@ async def get_user(users_id:int,db:Session=Depends(get_db)):
 
 # insert
 @app.post("/users",response_model=validation.UsersInsert)
-async def insert_user(user_name:str, departments_id:int, db:Session=Depends(get_db)):
+async def index_user(user_name:str, departments_id:int, db:Session=Depends(get_db)):
     """
         crud_users.pyの関数でsqlを実行する
         POSTでクエリパラメーターで指定したnameとdepartments_idで新たに追加する
@@ -102,6 +102,6 @@ async def insert_user(user_name:str, departments_id:int, db:Session=Depends(get_
         curl -X POST "http://localhost:8000/users?user_name=any&departments_id=1" -H "accept: application/json"
 
     """
-    return i_users(user_name=user_name, departments_id=departments_id, db=db)
+    return insert_user(user_name=user_name, departments_id=departments_id, db=db)
 
 
